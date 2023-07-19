da639620598aa03a604a464d072ffa97c99a5636
Deepak <dgusaiwal537@gmail.com>
2023-07-19 23:14:09 +0530

    first commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="container heroSection">
+      <div className="contentBox">
+        <h1>YOUR FEET</h1>
+        <h1>DESERVE</h1>
+        <h1>THE BEST</h1>
+
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.
+        </p>
+
+        <div className="heroBtnGroup">
+          <button className="btn-primary">Shop Now</button>
+          <button className="btn-secondary">Category</button>
+        </div>
+
+        <div className="contentBottom">
+            <span>Also Available On</span>
+            <div className="imgGroupBox">
+                <img src="../public/img/flipkart.png" alt="flipkart.png" />
+                <img src="../public/img/amazon.png" alt="amazon.png" />
+            </div>
+        </div>
+      </div>
+
+      <div className="heroImgBox">
+        <img src="../public/img/hero-image.png" alt="hero-image.png" />
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <div className="container headerContainer">
+        <a href="/" className="logoBox">
+          <img src="../public/img/brand_logo.png" alt="brand_logo" />
+        </a>
+
+        <ul className="menu">
+          <li>
+            <a href="/">Menu</a>
+          </li>
+          <li>
+            <a href="/">Location</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+        </ul>
+
+        <button className="btn-primary">login</button>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
